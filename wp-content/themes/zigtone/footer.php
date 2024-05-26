7c63807d421b0e6d0b0bcd5d322de43d29534b5d
ctlaam <caothanhlam14092000@gmail.com>
2024-05-26 11:57:06 +0700

    frist commit

---
+++ wp-content/themes/zigtone/footer.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package zigtone
+ */
+
+?>
+<footer>
+	<p>Copyright © 2020 - 2024 by <a href="https://zigtone.com" title="Best Ringtones">ZIGTONE.COM</a>. All Rights Reserved.</p>
+	<p><a href="/contact-us" rel="nofollow">Contact Us</a> | <a href="/privacy-policy" rel="nofollow">Privacy Policy</a> | <a href="/cookie-policy" rel="nofollow">Cookie Policy</a> | <a href="/sitemap_index.xml" rel="nofollow">Sitemap</a> </p>
+	<ul class="social-link">
+		<li>
+			<a rel="nofollow" href="https://www.facebook.com/zigtone" title="facebook">Facebook</a>
+		</li>
+		<li>
+			<a rel="nofollow" href="https://www.youtube.com/bestringtonedownload" title="youtube">Youtube</a>
+		</li>
+		<li>
+			<a rel="nofollow" href="https://twitter.com/ToneZig" title="Twitter">Twitter</a>
+		</li>
+	</ul>
+	<style>
+		.social-link {
+			list-style: none;
+		}
+
+		.social-link li {
+			display: inline-block;
+			margin-right: 10px;
+		}
+
+		.social-link li a {
+			color: white;
+		}
+	</style>
+	<a href="https://www.dmca.com/Protection/Status.aspx?ID=bd20d1c7-1213-491f-90e3-ef6ccded2ae8&amp;refurl=https://zigtone.com/" target="_blank" rel="nofollow noopener noreferrer" title="DMCA.com Protection Status" class="dmca-badge"> <img src="https://images.dmca.com/Badges/dmca-badge-w100-5x1-06.png?ID=bd20d1c7-1213-491f-90e3-ef6ccded2ae8" alt="DMCA.com Protection Status"></a>
+	<script src="https://images.dmca.com/Badges/DMCABadgeHelper.min.js"> </script>
+</footer>
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
